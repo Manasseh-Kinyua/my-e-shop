0b41b65506ceef33990e5009e113ab16250934cd
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 16:48:29 +0300

    Add the relevant imports to the Product page

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 function ProductScreen() {
   return (
