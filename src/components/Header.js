f032f89ba7f8e5fb61410e845c0eabee59506606
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 16:39:56 +0300

    Reactify routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Navbar, Nav, Container, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
   return (
     <header>
         <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
             <Container>
-                <Navbar.Brand href="/">Nasseh E-Comm</Navbar.Brand>
+                <LinkContainer to='/'>
+                <Navbar.Brand>Nasseh E-Comm</Navbar.Brand>
+                </LinkContainer>
+
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
                     <Nav
@@ -14,8 +18,13 @@ function Header() {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                        <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
+                        <LinkContainer to='/cart'>
+                            <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+                        </LinkContainer>
+
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className='fas fa-user'></i>Login</Nav.Link>
+                        </LinkContainer>
                         {/* <Nav.Link href="#" disabled>
                         Link
                         </Nav.Link> */}
