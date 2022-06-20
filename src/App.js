5ef895e510c5b112cd5c0d3e90027ad8a26d9148
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 16:29:07 +0300

    Add the individual product page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { Container } from 'react-bootstrap'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header/>
         <main className='py-3'>
           <Container>
-            <HomeScreen/>
+            <Routes>
+              <Route path='/' element={<HomeScreen />}/>
+              <Route path='/product/:id' element={<ProductScreen />}/>
+            </Routes>
           </Container>
         </main>
       <Footer/>
-    </div>
+    </BrowserRouter>
   );
 }
 
