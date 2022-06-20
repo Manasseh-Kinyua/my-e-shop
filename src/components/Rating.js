560186567c62e327acac6d3d593f7a1fd3811c6e
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 16:03:33 +0300

    Add a star to the rating component

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -4,7 +4,13 @@ function Rating({value, text, color}) {
   return (
     <div className='rating'>
       <span>
-        <i style={color}>
+        <i style={{color}} className={
+            value >= 1
+            ? 'fas fa-star'
+            : value >= 0.5
+                ? 'fas fa-star-half-alt'
+                : 'far far-star'
+        }>
 
         </i>
       </span>
