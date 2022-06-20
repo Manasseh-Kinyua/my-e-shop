142d7c49a38f0529ab0468f970aad992abfcd4c3
Manasseh Kinyua <manasseh.kinyua@student.moringaschool.com>
2022-06-20 16:12:56 +0300

    style the rating components

--- src/components/Rating.js
+++ src/components/Rating.js
@@ -14,6 +14,52 @@ function Rating({value, text, color}) {
 
         </i>
       </span>
+      <span>
+        <i style={{color}} className={
+            value >= 2
+            ? 'fas fa-star'
+            : value >= 1.5
+                ? 'fas fa-star-half-alt'
+                : 'far far-star'
+        }>
+
+        </i>
+      </span>
+      <span>
+        <i style={{color}} className={
+            value >= 3
+            ? 'fas fa-star'
+            : value >= 2.5
+                ? 'fas fa-star-half-alt'
+                : 'far far-star'
+        }>
+
+        </i>
+      </span>
+      <span>
+        <i style={{color}} className={
+            value >= 4
+            ? 'fas fa-star'
+            : value >= 3.5
+                ? 'fas fa-star-half-alt'
+                : 'far far-star'
+        }>
+
+        </i>
+      </span>
+      <span>
+        <i style={{color}} className={
+            value >= 5
+            ? 'fas fa-star'
+            : value >= 4.5
+                ? 'fas fa-star-half-alt'
+                : 'far far-star'
+        }>
+
+        </i>
+      </span>
+
+      <span>{text && text}</span>
     </div>
   )
 }
